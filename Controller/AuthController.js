b4616e4484b294373b48cf636fe031ab16df6cfb
SinghSahitya <sahityasingh1211@gmail.com>
2024-11-28 23:46:36 +0530

    login

--- Controller/AuthController.js
+++ Controller/AuthController.js
@@ -1,4 +1,5 @@
 const bcrypt = require('bcrypt');
+const jwt = require("jsonwebtoken");
 const BusinessModel = require("../models/Business");
 
 
@@ -18,4 +19,33 @@ const signup = async (req, res) => {
     }
 }
 
-module.exports = {signup}
+
+const login = async (req, res) => {
+    try{
+        const { email, password } = req.body;
+        const user = await BusinessModel.findOne((email));
+        const errorMsg = "Auth failed either email or password is wrong" 
+        if (!user){
+            return res.status(403).json({ message:errorMsg, success:false  });
+        }
+
+        const isPassEqual = await bcrypt.compare(password, user.passowrd);
+        
+        const jwtToken = jwt.sign(
+            { email:user.email, _id:user._id  },
+            process.env.JWT_SECRET,
+            {expiresIn:'24h'}
+        )
+
+        if (!isPassEqual){
+            return res.status(403).json({ message:errorMsg, success:false  });
+        }
+
+        res.status(201).json({ message:"Login successfully", success:true, jwtToken, email, name:user.b_name
+          });
+    }   catch (err){
+        res.status(500).json({ message:"Internal Error", success:false  });
+    }
+}
+
+module.exports = {signup,login}
--- Routes/AuthRouter.js
+++ Routes/AuthRouter.js
@@ -1,12 +1,12 @@
 const router = require('express').Router;
-const {  signup  } = require('../Controller/AuthController');
-const { signupValidation } = require('../Middleware/AuthValidation')
+const {  signup, login  } = require('../Controller/AuthController');
+const { signupValidation, loginValidation } = require('../Middleware/AuthValidation')
 
 router.post('/login', (req, res) => {
     res.send('login success');
 });
 
-
+router.post('/login', loginValidation, login);
 router.post('/signup', signupValidation, signup);
 
 module.exports = router;
